ff0084fc38bba69df26ae0aa2861ff680437ba88
PhilippeBas <geronimo8080@gmail.com>
2023-05-22 12:58:33 +0100

    La  1er function - le boutton

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import { MemeSVGViewer, emptyMeme } from 'orsys-tjs-meme';
+import Button from './components/ui/Button/Button';
 
 function App() {
   return (
     <div className="App">
       demat breizh
-      <MemeSVGViewer meme={emptyMeme} image={undefined}/>
+      <Button></Button>
     </div>
   );
 }
