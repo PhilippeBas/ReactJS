2834bb4d73ec70c0f6f7fcbfe7853c8d2664abaa
PhilippeBas <geronimo8080@gmail.com>
2023-05-22 11:11:53 +0100

    1er compoment

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import { MemeSVGViewer, emptyMeme } from 'orsys-tjs-meme';
 
 function App() {
   return (
     <div className="App">
       demat breizh
+      <MemeSVGViewer meme={emptyMeme} image={undefined}/>
     </div>
   );
 }
