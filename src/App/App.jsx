8068525c275270b3bcc4ece51e13d410d9472ad6
PhilippeBas <geronimo8080@gmail.com>
2023-05-23 15:55:40 +0100

    Affichage  de l

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -4,7 +4,7 @@ import './App.css';
 import FlexW1Grow from './components/layout/FlexW1Grow/FlexW1Grow';
 import Header from './components/ui/Header/Header';
 import Footer from './components/ui/Footer/Footer';
-import { MemeSVGViewer, emptyMeme } from 'orsys-tjs-meme'
+import { MemeSVGViewer, emptyMeme, MemeSVGThumbnail } from 'orsys-tjs-meme';
 import MemeForm from './components/functionnal/MemeForm/MemeForm';
 import FlexH3Grow from './components/layout/FlexH3Grow/FlexH3Grow';
 import NavBar from './components/ui/NavBar/NavBar';
@@ -13,23 +13,36 @@ import NavBar from './components/ui/NavBar/NavBar';
 function App(props) {
   const [meme, setmeme] = useState(emptyMeme);
   const [imgs, setimgs] = useState([]);
+  const [memes,setmemes] = useState([]);
+
   useEffect(() => {
-    fetch('http://localhost:5679/images')
+    const prImg=fetch('http://localhost:5679/images')
+     .then(r => r.json())
+      
+      const prMeme=fetch('http://localhost:5679/memes')
       .then(r => r.json())
-      .then(arr => setimgs(arr));
+
+      Promise.all([prImg,prMeme]).then(arr=>{
+        setimgs(arr[0]);
+        setmemes(arr[1]);
+      })
+       
   }, [])
   return (
 
     <FlexH3Grow>
       <Header />
       <NavBar />
-      {JSON.stringify(meme)}
-      <FlexW1Grow>
+      {/* {JSON.stringify(meme)} */}
+
+      <MemeSVGThumbnail memes={memes} images={imgs} basePath=''/>
+
+      {/* <FlexW1Grow>
         <MemeSVGViewer meme={meme} image={imgs.find((img, pos) => img.id === meme.imageId)} basePath='' />
         <MemeForm meme={meme} images={imgs} onMemeChange={(meme) => {
           setmeme(meme)
         }} />
-      </FlexW1Grow>
+      </FlexW1Grow> */}
       <Footer />
     </FlexH3Grow>
 
