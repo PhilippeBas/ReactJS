8e6cdda7794124035b66df8cb6badfde95939c8d
PhilippeBas <geronimo8080@gmail.com>
2023-05-23 15:09:37 +0100

    gestion imagee

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -4,7 +4,7 @@ import './App.css';
 import FlexW1Grow from './components/layout/FlexW1Grow/FlexW1Grow';
 import Header from './components/ui/Header/Header';
 import Footer from './components/ui/Footer/Footer';
-import {MemeSVGViewer ,emptyMeme} from 'orsys-tjs-meme'
+import { MemeSVGViewer, emptyMeme } from 'orsys-tjs-meme'
 import MemeForm from './components/functionnal/MemeForm/MemeForm';
 import FlexH3Grow from './components/layout/FlexH3Grow/FlexH3Grow';
 import NavBar from './components/ui/NavBar/NavBar';
@@ -12,20 +12,21 @@ import NavBar from './components/ui/NavBar/NavBar';
 
 function App(props) {
   const [meme, setmeme] = useState(emptyMeme);
+  const [imgs, setimgs] = useState([])
   return (
 
-  <FlexH3Grow>
-    <Header />
-    <NavBar />
-    {JSON.stringify(meme)}
-    <FlexW1Grow>
-        <MemeSVGViewer meme={meme} image={undefined} basePath=''/>
-        <MemeForm meme={meme} onMemeChange={(meme)=>{
+    <FlexH3Grow>
+      <Header />
+      <NavBar />
+      {JSON.stringify(meme)}
+      <FlexW1Grow>
+        <MemeSVGViewer meme={meme} image={imgs.find((img, pos) => imgs.at.id === meme.imageId)} basePath='' />
+        <MemeForm meme={meme} images={imgs} onMemeChange={(meme) => {
           setmeme(meme)
-        }}/>
-    </FlexW1Grow>
-    <Footer/>
-  </FlexH3Grow>
+        }} />
+      </FlexW1Grow>
+      <Footer />
+    </FlexH3Grow>
 
   );
 }
