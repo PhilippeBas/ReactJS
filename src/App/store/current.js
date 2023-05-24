0cdebaca2ef2e7a4c695ddea36e7fb67b6d9382e
PhilippeBas <geronimo8080@gmail.com>
2023-05-24 14:12:37 +0100

    Savegarde

--- src/App/store/current.js
+++ src/App/store/current.js
@@ -2,6 +2,7 @@
 import { emptyMeme } from "orsys-tjs-meme";
 
 import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk } from '@reduxjs/toolkit'
 
 const initialState=emptyMeme;
 
@@ -17,6 +18,17 @@ const current = createSlice({
     }
   }
 });
+
+export const saveCurrent=createAsyncThunk('current/save',async(meme)=>{
+    const pr=await fetch(`http://localhost:5679/memes${undefined !== meme.id ? '/' +meme.id: ''}`,{
+        method:undefined!==meme.id?'put':'post',
+        headers:{
+            "Content-Type":"Application/json"
+        },
+        body: JSON.stringify(meme)
+    })
+    return await pr.json();
+})
 
 export const {changeMeme,clearMeme} = current.actions
 
